5195a07354923465897fa8febeb6805eb8f0bf51
lysialeao <lysialeao@gmail.com>
2022-05-17 10:18:04 -0300

    Add database

---
+++ database.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE IF NOT EXISTS gso;
+USE gso;
+
+CREATE TABLE Pessoa(
+    id_pessoa INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    nascimento DATE NULL
+);
+
--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-from flask import Flask
-import jsonfy
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route("/", method=["GET"])
+@app.route("/", methods=["GET"])
 
 def root_path():
-    return jsonfy(msg="It's running!", code=200)
+    return jsonify(msg="It's running!", code=200)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=43333)

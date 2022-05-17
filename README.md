c483c5f81c5eb423aaed7bf3e0dc62b9b435ea75
lysialeao <lysialeao@gmail.com>
2022-05-17 11:30:58 -0300

    Mysql connector

---
+++ README.md
@@ -0,0 +1,2 @@
+#Aplicação
+Projeto desenvolvido durante as aulas de GSO ministrada pelo professor Lucaz Venezian, para o 5º período de Análise de Sistemas no IFSP Jacareí 2022.
--- main.py
+++ main.py
@@ -1,11 +1,46 @@
+import mysql.connector
+
+def insert_random(name, birthdate):
+    mydb = mysql.connector.conect(
+        user="aluno",
+        password="aluno123",
+        database="gso"
+    )
+
+    mycursor = mydb.cursor()
+
+    mycursor.execute(
+        'insert into Pessoa (nome, nascimento) VALUES (%s, %s)', values
+    )
+
+    mydb.commit()
+
+
+import names
+
+from faker import Faker
 from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
-
 def root_path():
     return jsonify(msg="It's running!", code=200)
+
+@app.route("/pessoa/random", methods=["POST"])
+def random_person():
+
+    name = names.get_full_name()
+    birthdate = Faker().date_between(
+        start_date="-100y", end_date="-18y")
+
+    response = {
+        'nome' : name,
+        'nascimento' :  birthdate
+    }
+
+    print(name, birthdate)
+    return jsonify(response)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=43333)
